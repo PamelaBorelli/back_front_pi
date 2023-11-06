0beed860ba50f0a3ddc57eb25bf5cf7a281ccc3d
PamelaBorelli <102415224+PamelaBorelli@users.noreply.github.com>
2023-11-06 16:45:34 -0300

    .

--- backend/app.js
+++ backend/app.js
@@ -13,6 +13,6 @@ conn()
 const routes = require("./routes/router")
 app.use("/api", routes)
 
-app.listen(5000, function (){
+app.listen(3000, function (){
     console.log("servidor online")
 })
--- backend/db/conn.js
+++ backend/db/conn.js
@@ -1,10 +1,17 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+require('dotenv').config(); 
 
 async function main(){
     try {
         mongoose.set("strictQuery", true)
 
-        await mongoose.connect("mongodb+srv://pcborelli:Samhaim12@cluster0.zoh2gvj.mongodb.net/?retryWrites=true&w=majority")
+        const user = process.env.MONGO_USER; // Acessa a variável de ambiente MONGO_USER
+        const pass = process.env.MONGO_PASS; // Acessa a variável de ambiente MONGO_PASS
+
+        // Usa as variáveis de ambiente na string de conexão
+        const uri = `mongodb+srv://${user}:${pass}@cluster0.zoh2gvj.mongodb.net/?retryWrites=true&w=majority`;
+
+        await mongoose.connect(uri); // Conecta ao MongoDB usando a string de conexão segura
 
         console.log("Conectado ao Banco!");
 
